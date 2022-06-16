8abb84f5b6ee1898dad9b7e401fa8a05123953d4
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-16 10:42:45 +0800

    基础复习

--- 复习01/04-copy函数.go
+++ 复习01/04-copy函数.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main04() {
+	data := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s1 := data[8:] //[8,9]
+	fmt.Println("s1=", s1)
+	s2 := data[0:5] //[0,1,2,3,4]
+	fmt.Println("s2=", s2)
+	copy(s2, s1)
+	fmt.Println("拷贝后：s2=", s2)
 }
