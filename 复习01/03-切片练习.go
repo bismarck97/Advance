9148798144c8f14abba7cbb32e1a5b3fe1f639c8
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-16 10:11:46 +0800

    基础复习

--- 复习01/03-切片练习.go
+++ 复习01/03-切片练习.go
@@ -1,15 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //练习1：给定一个字符串列表，在原有slice上返回不包含空字符串的列表， 如：
 //{"red", "", "black", "", "", "pink", "blue"}
 //——> {"red", "black", "pink", "blue"}
+//练习2：写一个函数，就地消除[]string中重复字符串，如：
+//{"red", "black", "red", "pink", "blue", "pink", "blue"}
+//——>	{"red", "black", "pink", "blue"}
 
-func main03() {
+func main0301() {
 	data := []string{"red", "", "black", "", "", "pink", "blue"}
-	afterData := noEmpty(data)
+	afterData := noEmpty2(data)
 	fmt.Println(afterData)
+}
+func main0302() {
+	data := []string{"red", "black", "red", "pink", "blue", "pink", "blue"}
+	afterData := noRepetition(data)
+	fmt.Println(afterData)
+}
+
+func noRepetition(data []string) []string {
+	//out := make([]string, 1)
+	//out[0] = data[0]
+	out := data[:1]
+	//遍历原始切片字符串
+	for _, str := range data {
+		//设置flag标志位
+		i := 0
+		//比较取出的str是否存在
+		for ; i < len(out); i++ {
+			if str == out[i] {
+				//如果重复就跳出本次循环
+				break
+			}
+		}
+		//判断循环次数是否等于切片长度，如果等于说明循环正常结束，没有经过break跳过，可以把数据值填入
+		if i == len(out) {
+			out = append(out, str)
+		}
+	}
+	return out
 }
 
 ///练习：给定一个字符串列表，在原有slice上返回不包含空字符串的列表， 如：
@@ -21,4 +54,20 @@ func noEmpty(data []string) []string {
 		}
 	}
 	return out
+}
+
+//不使用append直接在原切片上操作
+func noEmpty2(data []string) []string {
+	//计数器
+	i := 0
+	for _, str := range data {
+		if str != "" {
+			//不等于空的话从第一个开始写入
+			data[i] = str
+			//每符合一个条件加一
+			i++
+		}
+	}
+	//返回到条件结束时的切片
+	return data[:i]
 }
