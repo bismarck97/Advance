a60b6502988e71d4de86e6ce7d6af2641976236d
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-16 15:36:02 +0800

    基础复习

--- 复习01/05-拷贝练习.go
+++ 复习01/05-拷贝练习.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
 //练习3：要删除slice中间的某个元素并保存原有的元素顺序， 如：
 //{5, 6, 7, 8, 9} ——> {5, 6, 8, 9}
 
-func main() {
+func main05() {
+	s1 := []int{5, 6, 7, 8, 9}
+	s2 := remove(s1, 2)
+	fmt.Println(s2)
+}
 
+func remove(data []int, index int) []int {
+	//从index开始，截取+1拷贝到切片中
+	copy(data[index:], data[index+1:])
+	return data[:len(data)-1]
 }
