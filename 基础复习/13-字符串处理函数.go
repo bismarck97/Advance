82cfc705048f31b3e32fdaa040f30c40c9184f66
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-17 18:46:22 +0800

    基础复习

--- 基础复习/13-字符串处理函数.go
+++ 基础复习/13-字符串处理函数.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	str := "I love my work and I love my family too"
+	s := strings.Split(str, " ")
+	fmt.Println(s)
 }
