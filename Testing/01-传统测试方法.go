b20d22deceaf1a876f2fb40dd9a74b7b40ef10b2
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-26 22:07:23 +0800

    单元测试

--- Testing/01-传统测试方法.go
+++ Testing/01-传统测试方法.go
@@ -1,1 +1,23 @@
-package Testing
+package main
+
+import (
+	"fmt"
+)
+
+//有一个被测试的函数
+func addUpper(n int) int {
+	res := 0
+	for i := 1; i <= n; i++ {
+		res += i
+	}
+	return res
+}
+func main() {
+	//传统的测试方法，就是在main函数中使用看看结果是否正确
+	res := addUpper(10)
+	if res != 55 {
+		fmt.Printf("addUpper错误 返回=%v 期望值=%v\n", res, 55)
+	} else {
+		fmt.Printf("addUpper正确 返回=%v 期望值=%v\n", res, 55)
+	}
+}
